19650e231c3ec818671f55325f57527dc8da1531
Jose Diaz <jose.diaz@joedayz.pe>
2015-05-22 20:20:26 -0500

    Cliente Consume WS

---
+++ cliente/src/main/java/cliente/Application.java
@@ -0,0 +1,23 @@
+package cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+import cliente.wsdl.GetCityForecastByZIPResponse;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = SpringApplication.run(WeatherConfiguration.class, args);
+
+		WeatherClient weatherClient = ctx.getBean(WeatherClient.class);
+
+		String zipCode = "94304";
+		if (args.length > 0) {
+			zipCode = args[0];
+		}
+		GetCityForecastByZIPResponse response = weatherClient.getCityForecastByZip(zipCode);
+		weatherClient.printResponse(response);
+	}
+}
---
+++ cliente/src/main/java/cliente/WeatherConfiguration.java
@@ -0,0 +1,27 @@
+
+package cliente;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class WeatherConfiguration {
+
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("cliente.wsdl");
+		return marshaller;
+	}
+
+	@Bean
+	public WeatherClient weatherClient(Jaxb2Marshaller marshaller) {
+		WeatherClient client = new WeatherClient();
+		client.setDefaultUri("http://wsf.cdyne.com/WeatherWS/Weather.asmx");
+		client.setMarshaller(marshaller);
+		client.setUnmarshaller(marshaller);
+		return client;
+	}
+
+}
